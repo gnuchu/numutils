5663519a25905d8b0149b992c6275a760bbe8e00
John Griffiths <gnuchu@gmail.com>
2021-12-23 23:22:25 +0000

    Bump version: 0.1.0 → 0.1.0

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     package_data={'numutils': ['config/**/*.yaml']},
     test_suite='tests',
     url='https://github.com/gnuchu/numutils.n2w',
-    version = '0.1.0',
+    version = 'version = '0.1.0'',
     zip_safe=False,
 )
