d7fb004e4c9f31373a99f6c59dff58dfc649386f
John Griffiths <gnuchu@gmail.com>
2022-01-02 23:40:10 +0000

    Correct typo

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,10 +4,12 @@ pipeline {
     stage('Run Tests') {
       steps {
         script {
+          sh 'make test-all'
+
           withPythonEnv('/usr/bin/python3.8') {
             sh 'pip install -r requirements.txt'
             sh 'make test'
-            sh 'mske lint'
+            sh 'make lint'
           }
           withPythonEnv('/usr/bin/python3.9') {
             sh 'pip install -r requirements.txt'
